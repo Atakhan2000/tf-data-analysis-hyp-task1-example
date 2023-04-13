1333f20221346c089ae74e180af1971ff5a849c9
Atakhan2000 <57829588+Atakhan2000@users.noreply.github.com>
2023-04-13 15:38:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
+from statsmodels.stats.proportion import proportions_ztest
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 653318045 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +13,11 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha=0.09
+    
+    p_value=proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')[1]
+    print(p_value)
+    if (p_value < alpha) and (x_success/x_cnt <= y_success/y_cnt):
+      return True
+    else: 
+      return False
